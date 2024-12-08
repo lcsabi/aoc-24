cab609c4e46812824198ec00d31e6642930eccb6
Laszlo Csaba <laszlo.cs4ba@gmail.com>
2024-12-08 21:13:31 +0100

    implement Part One of Day 2

--- day02/day02.go
+++ day02/day02.go
@@ -1,9 +1,51 @@
 package main
 
 import (
+	"aoc-24/utils"
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Hello, world!")
+	safeCount := 0
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		strs := strings.Split(scanner.Text(), " ")
+		fst, _ := strconv.Atoi(strs[0])
+		snd, _ := strconv.Atoi(strs[1])
+		if fst == snd {
+			continue
+		}
+		if utils.AbsDiff(fst, snd) > 3 {
+			continue
+		}
+		idx := 1
+		if fst > snd { // Decreasing
+			for idx < len(strs)-1 {
+				left, _ := strconv.Atoi(strs[idx])
+				right, _ := strconv.Atoi(strs[idx+1])
+				if left <= right || left-right > 3 {
+					break
+				}
+				idx++
+			}
+		} else { // Increasing
+			for idx < len(strs)-1 {
+				left, _ := strconv.Atoi(strs[idx])
+				right, _ := strconv.Atoi(strs[idx+1])
+				if left >= right || left-right < -3 {
+					break
+				}
+				idx++
+			}
+		}
+		if idx == len(strs)-1 {
+			safeCount++
+		}
+	}
+
+	fmt.Println(safeCount) // Part One
 }
